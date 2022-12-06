104cb967a22e78b2c11ba6283d5dff4f85c0877e
MBallGCC <ballmd20@gcc.edu>
2022-12-05 22:36:35 -0500

    - FIXED AJAX. - Profile will now load a preview of 4 posts and 4 comments (if there     are that many, otherwise just all of them) - Added tojson() to all of the SQL Tables to make it easier for AJAX

--- static/styles/profile_details.js
+++ static/styles/profile_details.js
@@ -4,52 +4,64 @@ document.addEventListener("DOMContentLoaded", async () => {
 });
 
 async function loadPostPreview() {
-    const postDir = `/profile/${userid}/jsondump/`;
-    //FIGURE OUT HOW TO TRANSFER USERID TO THIS. NEEDS IT.
+    const userid = document.getElementById("usernum").getAttribute("data")
+    const infoDir = `/profile/${userid}/jsondump/`;
     const postDiv = document.getElementById("post-div");
 
-    fetch(postDir)
-        .then(posts => {
+    fetch(infoDir)
+        .then(validateJSON)
+        .then(data => {
             for (let i=0; i<4; i++) {
-                if (posts[i] !== null) {
+                if (!(typeof data.posts[i] == "undefined")) {
                     const hr = document.createElement("hr");
                     const title = document.createElement("h2");
-                    title.innerText = `${posts[i].post_name}`;
+                    title.innerText = `${data.posts[i].post_name}`;
                 
                     const preview = document.createElement("p");
-                    const text = `${posts[i].recipe}`.substring(0,50) + "...";
+                    const text = `${data.posts[i].recipe}`.substring(0,50) + "...";
                     preview.innerText = text;
                     postDiv.append(hr, title, preview);
                 }
             }
             const hr = document.createElement("hr");
-            postDiv.appendChild("hr");
+            postDiv.appendChild(hr);
         })
         .catch(error => {console.log(`Error occurred: ${error}`)})
 }
 
 async function loadCommentPreview() {
-    const comments = document.getElementById("getscript").getAttribute("comments");
+    const userid = document.getElementById("usernum").getAttribute("data")
+    const infoDir = `/profile/${userid}/jsondump/`;
     const commentDiv = document.getElementById("comment-div");
-    const commentTEST = document.createElement("h1");
-    commentTEST.innerText("MADE IT HERE");
-    commentDiv.appendChild(commentTEST);
 
-    for (let i=0; i<4; i++) {
-        if (comments[i] !== null) {
-            const hr = document.createElement("hr");
-            const title = document.createElement("h2");
-            title.innerText = `${comments[i].post_id}`;
+    fetch(infoDir)
+        .then(validateJSON)
+        .then(data => {
+            for (let i=0; i<4; i++) {
+                if (!(typeof data.comments[i] == "undefined")) {
+                    const hr = document.createElement("hr");
+                    const title = document.createElement("h2");
+                    title.innerText = `${data.comments[i].postinfo.post_name}`;
+                
+                    const preview = document.createElement("p");
+                    const text = `${data.comments[i].text}`.substring(1,50) + "...";
+                    preview.innerText = text;
         
-            const preview = document.createElement("p");
-            const text = `${comments[i].text}`.substring(1,50) + "...";
-            preview.innerText = text;
+                    const rating = document.createElement("p");
+                    rating.innerText = `${data.comments[i].rating}`;
+                    commentDiv.append(hr, title, preview, rating);
+                }
+            }
+            const hr = document.createElement("hr");
+            commentDiv.appendChild(hr);
+        })
+        .catch(error => {console.log(`Error occurred: ${error}`)})
+}
 
-            const rating = document.createElement("p");
-            rating.innerText = `${comments[i].rating}`;
-            commentDiv.append(hr, title, preview, rating);
-        }
+function validateJSON(response) {
+    if (response.ok) {
+        return response.json();
+    } else {
+        return Promise.reject(response);
     }
-    const hr = document.createElement("hr");
-    commentDiv.appendChild("hr");
 }
